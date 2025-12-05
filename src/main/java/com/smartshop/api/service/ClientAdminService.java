80e94ffbe280d11a085ba169189de2f7a1312556
achraf <achrafsikal@gmail.com>
2025-12-05 15:50:07 +0100

    feat: Add ClientAdminService for client management, including creation, retrieval, update, and deletion of clients

---
+++ src/main/java/com/smartshop/api/service/ClientAdminService.java
@@ -0,0 +1,63 @@
+package com.smartshop.api.service;
+
+import com.smartshop.api.dto.ClientRequestDTO;
+import com.smartshop.api.enums.UserRole;
+import com.smartshop.api.mapper.ClientMapper;
+import com.smartshop.api.model.Client;
+import com.smartshop.api.model.User;
+import com.smartshop.api.repository.ClientRepository;
+import com.smartshop.api.repository.UserRepository;
+import com.smartshop.api.util.PasswordUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import at.favre.lib.crypto.bcrypt.BCrypt;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class ClientAdminService {
+
+    private final ClientRepository clientRepository;
+    private final UserRepository userRepository;
+    private final ClientMapper clientMapper;
+    private final PasswordUtil passwordUtil;
+
+    public Client createClient(ClientRequestDTO dto) {
+
+        User user = new User();
+        user.setUsername(dto.getUserName());
+        System.out.println(dto);
+        String hashed = passwordUtil.hash(dto.getPassword());
+        user.setPassword(hashed);
+        user.setRole(dto.getRole());
+        userRepository.save(user);
+
+        Client client = clientMapper.toEntity(dto);
+        client.setUser(user);
+
+        return clientRepository.save(client);
+    }
+
+    public List<Client> getAllClients() {
+        return clientRepository.findAll();
+    }
+
+    public Client updateClient(UUID id, ClientRequestDTO dto) {
+        Client client = clientRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Client not found"));
+
+        clientMapper.updateEntity(dto, client);
+
+        return clientRepository.save(client);
+    }
+
+    public void deleteClient(UUID id) {
+        Client client = clientRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Client not found"));
+
+        userRepository.delete(client.getUser());
+        clientRepository.delete(client);
+    }
+}
