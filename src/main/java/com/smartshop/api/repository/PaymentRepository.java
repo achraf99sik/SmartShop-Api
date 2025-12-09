e388e95b024dafad2da405d2f6e9ba2a170ec31d
achraf <achrafsikal@gmail.com>
2025-12-09 14:24:05 +0100

    feat: Add method to retrieve maximum payment number in PaymentRepository

--- src/main/java/com/smartshop/api/repository/PaymentRepository.java
+++ src/main/java/com/smartshop/api/repository/PaymentRepository.java
@@ -2,10 +2,13 @@ package com.smartshop.api.repository;
 
 import com.smartshop.api.model.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.UUID;
 
 public interface PaymentRepository extends JpaRepository<Payment, UUID> {
     List<Payment> findPaymentsByOrderId(UUID orderId);
+    @Query("SELECT MAX(p.numeroPaiement) FROM Payment p")
+    Integer findMaxNumeroPaiement();
 }
